50fa5c183a0d82e17f803402f9abf6705d651ecd
Shukee <chidubemifeanyi40@gmail.com>
2023-09-08 00:05:46 +0100

    My C commit message~

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* 
+/** 
  *main - Entry Point
  *
  *Return: 0
@@ -9,7 +9,6 @@
 
 int main(void)
 {
-
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle";
 	return 0;
 }
