ad462fd2b159c8a5c38ea4146dc5e6b9c1285217
Shukee <chidubemifeanyi40@gmail.com>
2023-09-07 21:03:09 +0100

    My C commit message~

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-#include <stdio.h>
-int main() {
-    
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    
-    
-    
-    ;return 0;
+/*
+ * main - Entry point
+ *
+ * Return:0
+ */
+
+
+int main(void)
+{
+
+       	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
 }
