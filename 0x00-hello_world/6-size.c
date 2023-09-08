2556ae73f06b5f583a98794986b93928d959aa46
Shukee <chidubemifeanyi40@gmail.com>
2023-09-08 01:53:25 +0100

    My C commit message~

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,10 @@
 
 int main(void)
 {
-        printf("Size of char : %ld byte(s)\n", sizeof(char));
-        printf("Size of int : %ld byte(s)\n", sizeof(int));
-        printf("Size of a long int : %ld byte(s)\n", sizeof(long in\
-t));
-        printf("Size of long long int : %ld byte(s)\n", sizeof(long\
- long int));
-        printf("Size of float : %ld byte(s)\n", sizeof(float));
-        return 0;
+ printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+return (0);
 }
