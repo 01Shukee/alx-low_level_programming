dd77bf2b8cff9ccbb1751314525c117ff36f5b4e
Shukee <chidubemifeanyi40@gmail.com>
2023-09-08 22:48:26 +0100

    My C if, else, while commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,27 @@
-#include <stdio.h>
-
-/*
- * main - Entry Point
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch = '0';
-	while (ch <= 'f')
+	char ch = 'a';
+	int i = 48;
+	/* your code goes there */
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	i = 0;
+	while (i < 6)
 	{
 		putchar(ch);
 		ch++;
+		i++;
 	}
 	putchar('\n');
-        return (0);
+	return (0);
 }
