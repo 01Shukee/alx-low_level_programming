044b33b6e8f8bfcda59c683e065e8d906fcba326
Shukee <chidubemifeanyi40@gmail.com>
2023-09-14 16:53:37 +0100

    My More nested loops commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
+ */
+void print_square(int size)
+{
+	int i, j;
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		_putchar('#);
+	}
+	_putchar('\n');
+}
